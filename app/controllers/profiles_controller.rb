cfd6ae474c7239cdf2344ac088d6a2e3592735f7
Michael Barritt <michaelbarritt@hotmail.com>
2017-01-09 13:01:44 +0000

    Added user profiles form

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,3 +1,8 @@
 class ProfilesController < ApplicationController
-    
+  
+  # GET to /users/:user_id/profile/new
+  def new
+    # render blnak proflie details form
+    @profile = Profile.new
+  end
 end
